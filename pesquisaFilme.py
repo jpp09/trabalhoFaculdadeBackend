85061b54babc404d66ae4feae3ef53d3c2cffadf
João Pedro de Oliveira Martins <104173377+jpp09@users.noreply.github.com>
2024-11-21 23:20:58 -0300

    Realizando a criação da página de login

--- pesquisaFilme.py
+++ pesquisaFilme.py
@@ -2,12 +2,6 @@ from flask import Flask,redirect,request
 import requests
 
 
-url = "https://advanced-movie-search.p.rapidapi.com/search/movie"
-
-headers = {
-	"x-rapidapi-key": "681a22aa5cmsh35e4e17b7039abcp1b366bjsndb25fcbb2eb8",
-	"x-rapidapi-host": "advanced-movie-search.p.rapidapi.com"
-}
 
 app = Flask(__name__)
 
@@ -17,3 +11,5 @@ def buscar():
     querystring = {"query": nome_filme,"page":"1"}
     
     return f'O nome do filme é: {nome_filme}'
+
+app.run(debug=True)
