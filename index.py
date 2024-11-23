95d60f867592c64a49acc21ff5a6954a8c5ef232
João Pedro de Oliveira Martins <104173377+jpp09@users.noreply.github.com>
2024-11-23 09:12:09 -0300

    Criando o backend para a primeira página

--- index.py
+++ index.py
@@ -1,11 +1,20 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
-CORS(app)  # Habilita CORS para todas as rotas
 
-@app.route('/home', methods=['POST'])
+
+@app.route('/home')
 def home():
-    mensagem = 'Deu certo'
-    return jsonify({'mensagem': mensagem}), 200
+
+    url = "https://api.themoviedb.org/3/trending/all/day?language=en-US"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODZmNmQ2YzZkNTVhODJiNmY5MmU4NWE0ODc0MTljYyIsIm5iZiI6MTczMjM2MDAzMC45ODI5NjcsInN1YiI6IjY3MmU5M2U4N2ZkNzI0MzQyYTkwMDNhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qIYC-ShZPXNXYcJLc6dQZ_ohzXkscej-3yQterP3GZ0"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    return response.json()
 
