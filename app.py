50cceb315c6c007254ce438c3e04d0a1518249c4
João Pedro de Oliveira Martins <104173377+jpp09@users.noreply.github.com>
2024-11-23 09:49:22 -0300

    Concertando o CORS da aplicação

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/home')
 def home():
@@ -16,4 +18,3 @@ def home():
     response = requests.get(url, headers=headers)
 
     return jsonify(response.json())
-
