1c72e9ee77448390a369e4f6a695b14e9d84a712
João Pedro de Oliveira Martins <104173377+jpp09@users.noreply.github.com>
2024-11-25 22:02:43 -0300

    Finalizando a parte de login e cadastro no backend

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request,session
 from flask_cors import CORS
-import requests
+import requests,random
 
 app = Flask(__name__)
-CORS(app,origins=['https://jpp09.github.io'])
+'''CORS(app,origins=['https://jpp09.github.io'])'''
+CORS(app)
+
+cadastro_pessoas = {
+    'nome' : [],
+    'id' : [],
+    'email': [],
+    'usuario':[],
+    'senha':[]
+}
 
 @app.route('/home')
 def home():
@@ -19,3 +28,42 @@ def home():
 
     return jsonify(response.json())
 
+@app.route('/cadastro',methods=['POST'])
+
+def cadastro():
+    response = request.get_json()
+    cadastro_pessoas['nome'].append(response['nome'])
+    cadastro_pessoas['email'].append(response['email'])
+    cadastro_pessoas['usuario'].append(response['usuario'])
+    cadastro_pessoas['senha'].append(response['senha'])
+    id_gerador = f'{random.randint(0,100)}{random.randint(0,100)}'
+    if id_gerador in cadastro_pessoas['id']:
+        id_gerador = f'{random.randint(0,100)}{random.randint(0,100)}'
+    else:
+        cadastro_pessoas['id'].append(id_gerador)
+    return jsonify({'mensagem':f'O cadastro do {response['nome']} foi realizado com sucesso'})
+
+
+@app.route('/login',methods=['POST'])
+
+def login():
+    response = request.get_json()
+    usuario = response['user']
+    senha = response['password']
+    if usuario in cadastro_pessoas['usuario']:
+        index = cadastro_pessoas['usuario'].index(usuario)
+        if senha == cadastro_pessoas['senha'][index]:
+            return jsonify({'mensagem' : 'Login realizado com sucesso'})
+        else: 
+            return jsonify({'mensagem': 'Senha incorreta'})
+    else:
+        return jsonify({'mensagem': 'Usuário não detectado'})
+        
+
+ 
+
+@app.route('/add',methods=['POST',])
+def add():
+    pass
+
+app.run(debug=True)
